e1a547e4e7782501ff01256b1a8dbc470a2c6bfb
Marcelo Costa <mmiranda@ciandt.com>
2019-09-29 10:19:14 -0300

    Change json parsing logic

--- src/main/java/com/medium/hive/hook/CustomHook.java
+++ src/main/java/com/medium/hive/hook/CustomHook.java
@@ -1,5 +1,6 @@
 package com.medium.hive.hook;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.hadoop.hive.ql.QueryPlan;
 import org.apache.hadoop.hive.ql.hooks.ExecuteWithHookContext;
 import org.apache.hadoop.hive.ql.hooks.HookContext;
@@ -7,9 +8,12 @@ import org.apache.hadoop.hive.ql.hooks.HookContext.HookType;
 import org.apache.hadoop.hive.ql.hooks.ReadEntity;
 import org.apache.hadoop.hive.ql.hooks.WriteEntity;
 import org.apache.hadoop.hive.ql.plan.HiveOperation;
-import org.codehaus.jackson.map.ObjectMapper;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.apache.hadoop.hive.metastore.api.Database;
+import org.apache.hadoop.hive.ql.hooks.Entity;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -49,14 +53,29 @@ public class CustomHook implements ExecuteWithHookContext {
             Set<ReadEntity> inputs = hookContext.getInputs();
             Set<WriteEntity> outputs = hookContext.getOutputs();
 
-            ObjectMapper objMapper = new ObjectMapper();
+            for (Entity entity : inputs) {
+                logWithHeader("Hook metadata input value: " +  toJson(entity));
+            }
 
-            logWithHeader("Hook metadata input values: " + objMapper.writeValueAsString(inputs));
-            logWithHeader("Hook metadata output values: " + objMapper.writeValueAsString(outputs));
+            for (Entity entity : outputs) {
+                logWithHeader("Hook metadata output value: " +  toJson(entity));
+            }
 
         } else {
             logWithHeader("Non-monitored Operation, ignoring hook");
         }
+    }
+
+    private static String toJson(Entity entity) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        switch (entity.getType()) {
+            case DATABASE:
+                Database db = entity.getDatabase();
+                return mapper.writeValueAsString(db);
+            case TABLE:
+                return mapper.writeValueAsString(entity.getTable().getTTable());
+        }
+        return null;
     }
 
     private void logWithHeader(Object obj){
